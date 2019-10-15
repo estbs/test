f4e849a11c6ab66a1d9d50e2db1fa28e1af6459b
esteban <estbs93@gmail.com>
2019-10-14 22:44:42 -0500

    Add method that show all songs of an album
    Add method that show all songs of an album

--- app/controllers/api/v1/albums_controller.rb
+++ app/controllers/api/v1/albums_controller.rb
@@ -1,2 +1,9 @@
 class Api::V1::AlbumsController < ApplicationController
+
+	# GET /artists/:id/albums
+  def show
+  	album = Album.find(params[:id])
+    songs = album.songs
+    render json: songs, status: :ok
+  end
 end
