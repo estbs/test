0a0da36f7e571320ccd4361d8a637c594a57d2c3
esteban <estbs93@gmail.com>
2019-10-14 22:34:03 -0500

    Create methods on artist controller
    Create methods on artist controller

--- app/controllers/api/v1/artists_controller.rb
+++ app/controllers/api/v1/artists_controller.rb
@@ -1,2 +1,17 @@
 class Api::V1::ArtistsController < ApplicationController
+
+	# GET - /artists
+	def index
+    artists = Artist.all
+    render json: artists, status: :ok
+  end
+
+  # GET /artists/:id/albums
+  def show
+  	puts "enter show"
+  	artist = Artist.find(params[:id])
+    albums = artist.albums
+    render json: albums, status: :ok
+  end
+  
 end
