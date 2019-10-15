b15e413af4b56dc4e27fcd4292c8622bd01d809c
esteban <estbs93@gmail.com>
2019-10-14 22:44:00 -0500

    Create each route
    Create each route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
   	namespace :v1 do
-   		resources :artists, only: [:index, :show]
+  		get 'artists', to: 'artists#index'
+  		get 'artists/:id/albums', to: 'artists#show'
+  		get 'albums/:id/songs', to: 'albums#show'
   	end
  	end
 end
